2dea39aad5f67b8a4d5e4089308b3d550da65a00
Margaret Geary <gearymarge@gmail.com>
2020-07-19 00:04:42 +0000

    Completed addition

--- calculator.py
+++ calculator.py
@@ -3,5 +3,26 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-
+# repeat forever:
+def calculator_two():
+#     read input
+    string = input("Please enter a operation and numbers:")
+    #     tokenize input
+   ##print (string)
+    token = string.split(' ')
+    ###print (token)
+    #         ##if the first token is "q":
+    #print (token[0])
+    #print (int(token[1]) + int(token[2]))
+    if token[0] == "q":
+    #             quit
+        quit()
+    #         else:
+    else:
+        if token[0] == "+":
+            return add(int(token[1]), int(token[2]))
+    #             (decide which math function to call based on first token)
+    #             if the first token is 'pow':
+    #                   call the power function with the other two tokens
+print (calculator_two())
 # Replace this with your code
